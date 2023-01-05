6ef962e38d3e7678ddf10890ffae331977d70a04
Daniel <mrdanielhenricks@yahoo.com>
2023-01-05 00:34:54 -0600

    adding basic features

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Time from "./components/Time.svelte";
+  let testOne = {
+    name: "Rand",
+    openHour: 7,
+    openMinute: 30,
+    closedHour: 22,
+    closedMinute: 30,
+  };
+  let testTwo = {
+    name: "EBI",
+    openHour: 11,
+    openMinute: 0,
+    closedHour: 23,
+    closedMinute: 59,
+  };
+</script>
+
+<Time {...testOne} />
+<Time {...testTwo} />
---
+++ src/components/Time.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let name;
+  export let openHour;
+  export let closedHour;
+  export let openMinute;
+  export let closedMinute;
+  let d = new Date();
+  d = new Date();
+  let hour = d.getHours();
+  let minutes = d.getMinutes();
+  const checkTime = () => {
+    return (
+      (hour > openHour || (hour == openHour && minutes >= openMinute)) &&
+      (hour < closedHour || (hour == closedHour && minutes <= closedMinute))
+    );
+  };
+  const padTime = (hour, min) => {
+    let isAM = hour <= 11;
+    if (hour == 0) {
+      hour = 12;
+    }
+    return `${hour != 12 ? hour % 12 : hour}:${min < 10 ? "0" + min : min} ${
+      isAM ? "AM" : "PM"
+    }`;
+  };
+</script>
+
+<p>
+  The {name} dining hall opens at {padTime(openHour, openMinute)}
+  and closes at {padTime(closedHour, closedMinute)}. The current time is
+  {padTime(hour, minutes)}.
+  {name} is {checkTime() ? "open" : "closed"}.
+</p>
