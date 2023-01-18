a3cc7ac5ff61afd27287a877e3c148b04bb8c488
daniel <daniel.p.henricks@vanderbilt.edu>
2023-01-18 15:40:38 -0600

    removed unused styles and edited Schedules.svelte

--- src/components/Schedules.svelte
+++ src/components/Schedules.svelte
@@ -1,107 +1,95 @@
-<div id="center">
-    <div id="center2">
-        <div class="data">
-            <div class="container">
-                <div class="col-xs-3" />
-                <div class="col-xs-3">
-                    <table id="rounded-corner" summary="Dining">
-                        <thead>
-                            <tr>
-                                <th scope="col" class="rounded-company" />
-                                <th scope="col" class="rounded-q1">
-                                    <img
-                                        src="img/food.png"
-                                        height="20"
-                                        width="20"
-                                    /> Food
-                                </th>
-                                <th scope="col" class="rounded-q4" />
-                            </tr>
-                        </thead>
-                        <tfoot>
-                            <tr>
-                                <th>
-                                    Times change during breaks &
-                                    <a
-                                        href="http://campusdining.vanderbilt.edu/vu-meal-plans/special-events/"
-                                        >events</a
-                                    >. Click for
-                                    <a
-                                        href="http://campusdining.vanderbilt.edu/menus/"
-                                        >menus</a
-                                    >.
-                                </th>
-                            </tr>
-                        </tfoot>
-                        <tbody>
-                            <tr id="link1">
-                                <td><img id="open1" src="" /></td>
-                                <td>Rand Dining Center</td>
-                                <td id="1" />
-                            </tr>
-                            <tr id="link2">
-                                <td><img id="open2" src="" /></td>
-                                <td>EBI</td>
-                                <td id="2" />
-                            </tr>
-                            <tr id="link3">
-                                <td><img id="open3" src="" /></td>
-                                <td>The Commons</td>
-                                <td id="3" />
-                            </tr>
-                            <tr id="link4">
-                                <td><img id="open4" src="" /></td>
-                                <td>The Pub</td>
-                                <td id="4" />
-                            </tr>
-                            <tr id="link5">
-                                <td><img id="open5" src="" /></td>
-                                <td>Rothschild</td>
-                                <td id="5" />
-                            </tr>
-                            <tr id="link6">
-                                <td><img id="open6" src="" /></td>
-                                <td>Zeppos</td>
-                                <td id="5" />
-                            </tr>
-                            <tr id="link12">
-                                <td><img id="open12" src="" /></td>
-                                <td>Suzie's Cafe: Blair & FGH</td>
-                                <td id="12" />
-                            </tr>
-                            <tr id="link13">
-                                <td><img id="open13" src="" /></td>
-                                <td>Suzie's Cafe: MRB III</td>
-                                <td id="13" />
-                            </tr>
-                            <tr id="link14">
-                                <td><img id="open14" src="" /></td>
-                                <td>Food for Thought Cafe</td>
-                                <td id="14" />
-                            </tr>
-                            <tr id="link15">
-                                <td><img id="open15" src="" /></td>
-                                <td>Local Java</td>
-                                <td id="15" />
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+<div class="supporting">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <table id="rounded-corner" summary="Dining">
+                    <thead>
+                        <tr>
+                            <th scope="col" class="rounded-company" />
+                            <th scope="col" class="rounded-q1">
+                                <img
+                                    src="../images/food.png"
+                                    height="20"
+                                    width="20"
+                                    alt="Food"
+                                /> Food
+                            </th>
+                            <th scope="col" class="rounded-q4" />
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr id="link1">
+                            <td><img id="open1" src="" /></td>
+                            <td>Rand Dining Center</td>
+                            <td id="1" />
+                        </tr>
+                        <tr id="link2">
+                            <td><img id="open2" src="" /></td>
+                            <td>EBI</td>
+                            <td id="2" />
+                        </tr>
+                        <tr id="link3">
+                            <td><img id="open3" src="" /></td>
+                            <td>The Commons</td>
+                            <td id="3" />
+                        </tr>
+                        <tr id="link4">
+                            <td><img id="open4" src="" /></td>
+                            <td>The Pub</td>
+                            <td id="4" />
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-sm-3" />
+            <div class="col-sm-3">
+                <table id="rounded-corner" summary="Dining">
+                    <thead>
+                        <tr>
+                            <th scope="col" class="rounded-company" />
+                            <th scope="col" class="rounded-q1">
+                                <img
+                                    src="../images/food.png"
+                                    height="20"
+                                    width="20"
+                                /> Food
+                            </th>
+                            <th scope="col" class="rounded-q4" />
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr id="link1">
+                            <td><img id="open1" src="" /></td>
+                            <td>Rand Dining Center</td>
+                            <td id="1" />
+                        </tr>
+                        <tr id="link2">
+                            <td><img id="open2" src="" /></td>
+                            <td>EBI</td>
+                            <td id="2" />
+                        </tr>
+                        <tr id="link3">
+                            <td><img id="open3" src="" /></td>
+                            <td>The Commons</td>
+                            <td id="3" />
+                        </tr>
+                        <tr id="link4">
+                            <td><img id="open4" src="" /></td>
+                            <td>The Pub</td>
+                            <td id="4" />
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
 </div>
 
 <style type="text/css">
-    .data .col-xs-3 {
-        top: -45px;
-        color: #fff;
-        margin-bottom: 40px;
-        margin-right: -30em;
-        right: 8px;
-    }
     ::-webkit-scrollbar {
-        width: 5px;
+        width: 10px;
         height: 3px;
     }
     ::-webkit-scrollbar-button {
@@ -113,24 +101,21 @@
         border: 0px none #ffffff;
         border-radius: 0px;
     }
-    ::-webkit-scrollbar-thumb:hover {
-        background: #ffffff;
-    }
     ::-webkit-scrollbar-thumb:active {
         background: #ffffff;
     }
-    ::-webkit-scrollbar-track {
-        background-color: rgba(43, 47, 54, 0.9);
-        border: 0px none #ffffff;
-        border-radius: 20px;
+    .container {
+        max-width: 56em;
+        justify-content: center;
     }
-    ::-webkit-scrollbar-track:hover {
-        background-color: rgba(43, 47, 54, 0.9);
+    .supporting {
+        text-shadow: 0 2px 3px rgba(0, 0, 0, 0.4);
     }
-    ::-webkit-scrollbar-track:active {
-        background-color: rgba(43, 47, 54, 0.9);
-    }
-    ::-webkit-scrollbar-corner {
-        background: none;
+
+    .supporting .col-xs-2 {
+        top: 0px;
+        text-align: center;
+        color: #fff;
+        margin-bottom: 1em;
     }
 </style>
